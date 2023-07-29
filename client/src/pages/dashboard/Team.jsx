0c5779559deb37a549e33e01a92f900bc494f513
shiva5152 <sg2304366@gmail.com>
2023-07-29 16:46:02 +0530

    favicon and users update

--- client/src/pages/dashboard/Team.jsx
+++ client/src/pages/dashboard/Team.jsx
@@ -2,8 +2,10 @@ import React ,{useEffect,useState} from 'react'
 import { showAlert, useAppContext } from '../../context/appContext';
 import {FormField, Loader } from '../../components';
 
+
 const SelectComponent = ({_id,role,updateUserRole}) => {
   const [isPopupOpen, setPopupOpen] = useState(false);
+  
   const togglePopup = () => {
     setPopupOpen(true);
   };
@@ -59,7 +61,7 @@ const SelectComponent = ({_id,role,updateUserRole}) => {
 
 const Team = () => {
  
-  const {isLoading,getAllUser,toggleAction,addUser,updateUserRole,deleteUser,allUsers}=useAppContext();
+  const {isLoading,getAllUser,toggleAction,addUser,updateUserRole,deleteUser,allUsers,isAdmin}=useAppContext();
   useEffect(() => {
     getAllUser();
   }, [toggleAction]);
@@ -74,7 +76,7 @@ const Team = () => {
 
   const openFormPopup = () => {
     setActiveFormPopup(true);
-  };
+  }; 
 
   const closeFormPopup = () => {
     setActiveFormPopup(false);
@@ -249,11 +251,15 @@ const Team = () => {
                             scope="row"
                             className="px-6 py-4  font-medium text-gray-900 whitespace-nowrap dark:text-black"
                           >
-                          <button onClick={()=>deleteUser(user._id)} className='w-full flex justify-center'>
+                          {user?.role!=="admin" ? (
+                            <button onClick={()=>deleteUser(user._id)} className='w-full flex justify-center'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="#5dffac" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                             </svg>
                           </button> 
+                          ):(
+                            "-"
+                          )}
                           </td> 
                           
                         </tr>
